4d1535746fa0d9538a8bf48ab03d5dc5a8d96cf5
K3ynyu <102559249+K3ynyu@users.noreply.github.com>
2022-03-28 19:32:46 -0300

    V1.0

--- NAN_detector.lua
+++ NAN_detector.lua
@@ -1,12 +1,22 @@
 
+-- Phelipe :D
+
+-- Define a variavel de players
 Players = game:GetService("Players")
 
+
+-- Inicia a lista de players
 for i, player in pairs(Players:GetPlayers()) do
     Patch = player.Name
     patch2 = game.Players[Patch].Dinheiro.Value
-    if patch2 > 50000000 then
+    
+    -- Abaixo Define-se o dinheiro de qualquer player for maior que 10 milhões sera imprimido uma mansagem estilo "warn" no console. (Com o Player_Nick e dinheiro exato.)
+    if patch2 > 10000000 then
+        -- Dentro desse if pode se adicionar qualquer execução para quem estiver com mais do que 10 milhôes (Exemplo: um script para resetar o player especifico.)
     warn (Patch, '|', patch2)
+        -- Abaixo está um codigo para especificar
     else
+        -- Nesse else fica pode se adicionar o codigo que acontece para os player que estiverem com menos de 10 milhôes.
         print (Patch, '|', patch2)
 end
 
